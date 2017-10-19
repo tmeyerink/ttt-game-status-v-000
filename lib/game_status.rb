80f33f90e05b0848c9d35eaac8fabd2911babfab
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-19 20:05:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,33 @@ def won?(board)
 WIN_COMBINATIONS.detect do |combinations|
   index_1 = combinations[0]
   index_2 = combinations[1]
-  index_3
-  
+  index_3 = combinations[2]
+
+  position_1 = board[index_1]
+  position_2 = board[index_2]
+  position_3 = board[index_3]
+
+  position_1 == position_2 && position_2 == position_3 && position_3 != " "
+  end
+end
+
+def full?(board)
+  board.all? do |complete|
+    complete != " "
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  won = won?(board)
+if won
+  board[won[0]]
+end
 end
