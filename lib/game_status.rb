82496673bbe27516366c9e8fc76e7baa76138753
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-19 19:58:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,5 +10,15 @@ WIN_COMBINATIONS = [
   [6,7,8],
   [0,4,8],
   [2,4,6],
-  
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
 ]
+
+def won?(board)
+WIN_COMBINATIONS.detect do |combinations|
+  index_1 = combinations[0]
+  index_2 = combinations[1]
+  index_3
+  
+end
