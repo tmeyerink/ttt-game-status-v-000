546fbb88919423fe25a47a10f952a01aa2339873
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-19 20:12:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,16 +25,15 @@ WIN_COMBINATIONS.detect do |combinations|
   position_2 = board[index_2]
   position_3 = board[index_3]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return combinations
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return combinations
-    else
-      false
-    end
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return combinations
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return combinations
+  else
+    false
   end
-  false
 end
+
 
 def full?(board)
   board.all? do |complete|
